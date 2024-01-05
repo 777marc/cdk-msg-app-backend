7c9d32de249ca3a89eea30b8cdaed6ed801c54e5
Marc Mendoza <777marc@gmail.com>
2024-01-05 14:51:53 -0500

    building up

--- lib/cdk-msg-app-backend-stack.ts
+++ lib/cdk-msg-app-backend-stack.ts
@@ -1,16 +1,40 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { RemovalPolicy, CfnOutput } from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class CdkMsgAppBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new dynamodb.Table(this, "Messages", {
+      partitionKey: {
+        name: "app_id",
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "created_at",
+        type: dynamodb.AttributeType.NUMBER,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkMsgAppBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new ec2.Vpc(this, "workshop-vpc", {
+      cidr: "10.1.0.0/16",
+      natGateways: 1,
+      subnetConfiguration: [
+        { cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC, name: "Public" },
+        {
+          cidrMask: 24,
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          name: "Private",
+        },
+      ],
+      maxAzs: 3, // Default is all AZs in region
+    });
+
+    new CfnOutput(this, "TableName", { value: table.tableName });
   }
 }
